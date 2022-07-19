8b537d39852fbe6d6af77719c36618cdae512ff1
Ednah1 <ednahosoro21g@gmail.com>
2022-07-19 13:23:20 -0700

    hot fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,3 +10,6 @@ void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+void print_chessboard(char (*a)[8]);
+
+#endif
